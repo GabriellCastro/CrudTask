1232a2ada38645dcfb9c71fa80ee98f15af0a57c
GabriellCastro <kastro.gabriel17@gmail.com>
2022-02-13 14:24:36 -0400

    error: gesture-handler ❌

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
+import Task from './src/pages/Task';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName='Task'
-      >
-
+      <Stack.Navigator initialRouteName='Task'>
+        <Stack.Screen
+          name='Task'
+          component={Task}
+        >
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
