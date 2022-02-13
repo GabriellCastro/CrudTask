d4ab8545b94ff0bb2a61a2dc8a09d14ed423d799
GabriellCastro <kastro.gabriel17@gmail.com>
2022-02-13 13:42:49 -0400

    add: config e inicializacao firebase 🦎

---
+++ src/config/firebaseconfig.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+import "firebase/storage";
+
+// A configuração do Firebase do seu app da Web
+const firebaseConfig = {
+  apiKey: "AIzaSyDZMbwD3TVF3Vdbkx-zbQZj71U1vzRPAY0",
+  authDomain: "task-7aac0.firebaseapp.com",
+  projectId: "task-7aac0",
+  storageBucket: "task-7aac0.appspot.com",
+  messagingSenderId: "30425358227",
+  appId: "1:30425358227:web:8c47a941132634835d9266"
+};
+
+// Inicializar o Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.firestore()
